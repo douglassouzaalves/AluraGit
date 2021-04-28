e1a3df92f74fbf05a8014868001a2a8d5719771d
Douglas Alves <meucontatodouglasalves@gmail.com>
2021-04-28 12:21:58 -0300

    nova mensagem adicionada

--- src/Home.java
+++ src/Home.java
@@ -1,6 +1,8 @@
 public class Home {
     public static void main(String[] args) {
-        System.out.printf("Testando....");
+        System.out.println("Testando....");
         System.out.println("Testando novamente");
+        System.out.println("Testando 3");
     }
 }
+
