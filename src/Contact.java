1476d87b10c6ba178f3cdd9e820eeb26d0a8e408
Douglas Alves <meucontatodouglasalves@gmail.com>
2021-04-28 12:55:07 -0300

    Adicionado e-mail para contato

--- src/Contact.java
+++ src/Contact.java
@@ -1,5 +1,6 @@
 public class Contact {
     public static void main(String[] args) {
         System.out.println("Entre em contato atráves do número 666-999");
+        System.out.println("Ou se preferir entre em contato pelo nosso e-mail teste@teste.com");
     }
 }
